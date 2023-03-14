6bd9e8073d1de77699828c67324f7a8e5557f01d
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-03-14 08:24:38 +0530

    Fisrt Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
 function App() {
+
+  const [movies, setMovies] = useState([        
+    {
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+    },
+    {
+        "Title": "Star Wars: Episode V - The Empire Strikes Back",
+        "Year": "1980",
+        "imdbID": "tt0080684",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Star Wars: Episode VI - Return of the Jedi",
+        "Year": "1983",
+        "imdbID": "tt0086190",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Star Wars: Episode VII - The Force Awakens",
+        "Year": "2015",
+        "imdbID": "tt2488496",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Star Wars: Episode I - The Phantom Menace",
+        "Year": "1999",
+        "imdbID": "tt0120915",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Star Wars: Episode III - Revenge of the Sith",
+        "Year": "2005",
+        "imdbID": "tt0121766",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+    <div className='row'>
+      <MovieList
+        movies={movies}
+      />
+    </div>      
     </div>
   );
 }
---
+++ src/components/MovieListHeading.jsx
@@ -0,0 +1,8 @@
+const MovieListHeading = (props) => {
+    return (
+    <div className="col">
+        <h1>{props.heading }</h1>
+
+    </div>
+    )
+}
